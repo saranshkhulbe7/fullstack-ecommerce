ffb60b97e794c45c322c6c18e2d06dfa4df54987
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2024-10-25 15:45:49 +0530

    random push

--- src/routes/auth/utils.ts
+++ src/routes/auth/utils.ts
@@ -3,6 +3,7 @@ type UserInfo = {
   id: number;
   role: string;
 };
+
 export const generateAccessToken = (userInfo: UserInfo) => {
   return jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
